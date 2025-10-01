a498ea38bcdbcf796e1d14903e1d4f52f2c61445
ST10326104 <130507907+ST10326104@users.noreply.github.com>
2025-10-01 22:38:00 +0200

    Update Rock.java

--- PRACTICAL-A1-ST10326104-Q2/src/rockpaperscissors/Rock.java
+++ PRACTICAL-A1-ST10326104-Q2/src/rockpaperscissors/Rock.java
@@ -1,7 +1,9 @@
 package RockPaperScissors;
+//References: Farrall, J., 2020, *Java Programming*, 9th ed.
 
 public class Rock extends Move {
     public Rock() {
         super("Rock", "Scissors");
     }
 }
+
