9abd9cf8393e505ab3a538a5201d795cd1202374
ST10326104 <130507907+ST10326104@users.noreply.github.com>
2025-10-01 22:38:24 +0200

    Update Scissors.java

--- PRACTICAL-A1-ST10326104-Q2/src/rockpaperscissors/Scissors.java
+++ PRACTICAL-A1-ST10326104-Q2/src/rockpaperscissors/Scissors.java
@@ -1,7 +1,9 @@
 package RockPaperScissors;
+//References: Farrall, J., 2020, *Java Programming*, 9th ed.
 
 public class Scissors extends Move {
     public Scissors() {
         super("Scissors", "Paper");
     }
 }
+
